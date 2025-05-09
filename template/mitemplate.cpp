8786134da8ea0dc595c9cdff19f882f008b93f1c
creefede <creefede@gmail.com>
2025-05-09 17:43:56 -0500

    Cambios en el template

--- template/mitemplate.cpp
+++ template/mitemplate.cpp
@@ -3,9 +3,16 @@
 #define fi first
 #define se second
 #define pb push_back
+#define el '\n'
 
 using namespace std;
 
 typedef long long ll;
 typedef double ld;
 typedef vector<int> vi;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+}

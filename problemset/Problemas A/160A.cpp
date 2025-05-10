95ba048ff650fbc5ac38ca3f0f5e92d34333c3bc
creefede <creefede@gmail.com>
2025-05-10 11:36:54 -0500

    160A terminar

---
+++ problemset/Problemas A/160A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef vector<int> vi;
+//Terminar
+int main(){
+  int n;
+  cin>>n;
+  unordered_map<int,int> count;
+  for(int i=0;i<n;++i){
+    int k;
+    cin>>k;
+    count[k] = count[k] + 1;
+  }
+  
+
+
+
+
+}

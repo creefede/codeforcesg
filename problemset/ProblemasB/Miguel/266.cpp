f348a98676c1f8f0b3c6b5ad26018fd1902b1cb9
Miguel-b <109560374+Miguel-b@users.noreply.github.com>
2025-05-15 23:01:35 -0500

    Create 266.cpp

---
+++ problemset/ProblemasB/Miguel/266.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n,t;
+    cin>>n>>t;
+    string s;
+    cin>>s;
+    int i = 0,j=0;
+    while(j<t){
+        i = 0;
+        while(i<n){
+            if(s[i] == 'B' && s[i + 1] == 'G'){
+                swap(s[i],s[i+1]);
+                i++;
+            }
+            i++;
+        }
+        j++;
+    }
+    cout<<s;
+    return 0;
+}

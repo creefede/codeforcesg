fdc67cbea4bd4397238f9d771cc5e766d08937b5
Miguel-b <109560374+Miguel-b@users.noreply.github.com>
2025-05-26 11:57:46 -0500

    Update Not Quite_a_Palindromic_String.cpp

--- Contest/Round1027/Miguel/Not Quite_a_Palindromic_String.cpp
+++ Contest/Round1027/Miguel/Not Quite_a_Palindromic_String.cpp
@@ -1,1 +1,19 @@
-
+//---------------------------------Esta malo solo encuentra los pares correctos, no lo reorganiza
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n, t,p, i=0, j=0,cont=0;
+    string str;
+    cin>>t;
+    while(i<t){
+        cin>>n>>p>>str;
+        while(j<n/2){
+            if(str[j]==str[str.size()-j-1]){cont++;}
+            j++;
+        }if(cont==p){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+        i++;
+        j=0;
+        cont=0;
+    }
+}

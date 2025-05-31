64e3c69604b166b5dd4ca3e7b5677588a3819c0a
creefede <creefede@gmail.com>
2025-05-30 21:36:40 -0500

    Problema y borrando mode

---
+++ problemset/ProblemasA/Cree/155A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define pb push_back
+#define el '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int n;
+  cin>>n;
+  vi nums;
+  for(int i=0;i<n;++i){
+    int a;
+    cin>>a;
+    nums.pb(a);
+  }
+  int count = 0;
+  int min = nums[0], max = nums[0];
+
+  for(int i=1;i<n;++i){
+    if(nums[i]>max){
+      count++;
+      max = nums[i];
+    }else if(nums[i]<min){
+      count++;
+      min = nums[i];
+    }
+  }
+  cout<<count<<el;
+  return 0;
+}

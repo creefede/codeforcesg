2b02c8a50eac5dfe5bb279677c8cc008d7940a31
creefede <creefede@gmail.com>
2025-08-20 21:55:39 -0500

    Problema hecho

--- problemset/ProblemasA/Cree/160A.cpp
+++ problemset/ProblemasA/Cree/160A.cpp
@@ -3,25 +3,39 @@
 #define fi first
 #define se second
 #define pb push_back
+#define el '\n'
 
 using namespace std;
 
 typedef long long ll;
 typedef double ld;
 typedef vector<int> vi;
-//Terminar
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+
 int main(){
   int n;
   cin>>n;
-  unordered_map<int,int> count;
+  vi nums;
+  int platatotal = 0;
   for(int i=0;i<n;++i){
-    int k;
-    cin>>k;
-    count[k] = count[k] + 1;
+    int a;
+    cin>>a;
+    nums.pb(a);
+    platatotal += a;
   }
-  
-
-
-
-
+  sort(nums.begin(),nums.end(), greater<int>());
+  int count = 0;
+  int plata = 0;
+  for(int i=0;i<n;++i){
+    plata += nums[i];
+    count++;
+    platatotal -= nums[i];
+    if(plata>platatotal){
+      cout<<count<<el;
+      return 0;
+    }
+  }
+  cout<<count<<el;
+  return 0;
 }

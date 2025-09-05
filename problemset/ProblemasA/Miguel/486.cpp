fb86204f2c50c788bf5c2fcc0d9b343c3b4c3734
Miguel-b <109560374+Miguel-b@users.noreply.github.com>
2025-09-04 23:21:28 -0500

    Add files via upload

---
+++ problemset/ProblemasA/Miguel/486.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define pb push_back
+#define el '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  ll n;
+  cin>>n;
+  if(n%2==0){cout<<n/2;}
+  else{cout<<-(n+1)/2;}
+  return 0;
+}

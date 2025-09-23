6328b428962ac0ee99e95792c937ddc0a7f0ae28
Miguel-b <109560374+Miguel-b@users.noreply.github.com>
2025-09-23 13:10:57 -0500

    Add files via upload

---
+++ problemset/ProblemasA/Miguel/2009.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define pb push_back
+#define el '\n'
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int t; cin>>t;
+  while(t--){
+    int a, b; cin>>a>>b;
+    cout<<abs(a-b)<<el;
+  }
+}

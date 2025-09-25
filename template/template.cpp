add3d656fc48061da635318574b737a32e9cda40
Miguel-b <mbernalg2112@gmail.com>
2025-09-25 17:45:32 -0500

    .

--- template/template.cpp
+++ template/template.cpp
@@ -3,6 +3,7 @@
 #define fi first
 #define se second
 #define forn(i,n) for(int i=0;i< (int)n; ++i)
+#define foraut(p,m) for(auto &p : m)
 #define pb push_back
 #define el '\n'
 #define sz(v) int(v.size())
@@ -19,6 +20,8 @@ typedef vector<int> vi;
 typedef vector<ii> vii;
 typedef vector<ll> vll;
 typedef vector<ld> vd;
+typedef unordered_map<int,int> umi;
+typedef unordered_map<char,int> umc;
 
 int main(){
   ios_base::sync_with_stdio(false);

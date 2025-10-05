c6a42f352cecfb82f24ea210145deb7b9e2d819a
creefede <creefede@gmail.com>
2025-10-05 00:35:21 -0500

    Problemas

---
+++ problemset/ProblemasA/Cree/2152A.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0;i< (int)n; ++i)
+#define foraut(p,m) for(auto &p : m)
+#define pb push_back
+#define el '\n'
+#define sz(v) int(v.size())
+#define all(v) v.begin(),v.end()
+#define fa(x) bool x=false
+#define msg(x) cout << (x) << el
+#define bug(x) cout << (#x) << ": " << (x) << el
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+typedef unordered_map<int,int> umi;
+typedef unordered_map<char,int> umc;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int t;
+  cin>>t;
+  forn(z,t){
+    int n;
+    cin>>n;
+    vi tar;
+    forn(i,n){
+      int a;
+      cin>>a;
+      tar.pb(a);
+    }
+    int count = 0;
+    vi nums;
+    sort(all(tar));
+    forn(i,n){
+      auto hola = find(all(nums),tar[i]);
+      if(hola!=nums.end()){
+        continue;
+      }else{
+        nums.pb(tar[i]);
+        count++;
+      }
+    }
+    int ans = 2*count - 1;
+    cout<<ans<<el;
+  }
+  return 0;
+}
+

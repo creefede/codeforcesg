3d8b7b99ae1aa464c6456b38bb014cd10d30913a
creefede <creefede@gmail.com>
2025-10-06 20:40:59 -0500

    Problemas

---
+++ problemset/ProblemasA/Cree/581A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0;i< (int)n; ++i)
+#define foraut(p,m) for(auto &p : m)
+#define pb push_back
+#define el '\n'
+#define sz(v) int(v.size())
+#define all(v) v.begin(),v.end()
+#define fa(x) bool x=false
+#define msg(x) cout << (x) << el
+#define bug(x) cout << (#x) << ": " << (x) << el
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+typedef unordered_map<int,int> umi;
+typedef unordered_map<char,int> umc;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int a,b;
+  cin>>a>>b;
+  int parfa = min(a,b);
+  a -= parfa;
+  b -= parfa;
+  int counta = parfa;
+  int countb = 0;
+  if(a==0){
+    countb += b/2;
+  }else{
+    countb += a/2;
+  }
+  cout<<counta<<" "<<countb<<el;
+  return 0;
+}
+

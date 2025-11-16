5776b34e0b68978839828a84b77d50f6365dc595
Miguel <miguel@localhost.localdomain>
2025-11-15 22:42:53 -0500

    .

---
+++ Problemas/Codeforces/problemset/ProblemasB/Miguel/1353.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0;i< (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define foraut(p,m) for(auto &p : m)
+#define forc(c,s) for(char c: s)
+#define pb push_back
+#define el '\n'
+#define sz(v) int(v.size())
+#define all(v) v.begin(),v.end()
+#define tc int t; cin >> t; while(t--)
+#define fa(x) bool x=false
+#define d(x) cout << (x) << el
+#define bug(x) cout << (#x) << ": " << (x) << el
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+typedef unordered_map<int,int> umi;
+typedef unordered_map<char,int> umc;
+typedef unordered_map<string,int> ums;
+
+const int inf = 1e9;
+const int nax = 1e5+200;
+const ld pi = acos(-1);
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  tc{
+    int n,k;cin>>n>>k;
+    vi a(n);
+    vi b(n);
+    forn(i,n)cin>>a[i];
+    forn(j,n)cin>>b[j];
+    sort(all(a));
+    sort(all(b));
+    reverse(all(b));
+    forn(q,k){
+        if(a[q]<b[q])swap(a[q],b[q]);
+        else break;
+    }
+    ll cont=0;
+    forn(p,n)cont+=a[p];
+    d(cont);
+  }
+}
---
+++ Problemas/Codeforces/problemset/ProblemasB/Miguel/977.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0;i< (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define foraut(p,m) for(auto &p : m)
+#define forc(c,s) for(char c: s)
+#define pb push_back
+#define el '\n'
+#define sz(v) int(v.size())
+#define all(v) v.begin(),v.end()
+#define tc int t; cin >> t; while(t--)
+#define fa(x) bool x=false
+#define d(x) cout << (x) << el
+#define bug(x) cout << (#x) << ": " << (x) << el
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+typedef unordered_map<int,int> umi;
+typedef unordered_map<char,int> umc;
+typedef unordered_map<string,int> ums;
+
+const int inf = 1e9;
+const int nax = 1e5+200;
+const ld pi = acos(-1);
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int n;cin>>n;
+  string s;cin>>s;
+  ums ap;
+  forn(i,n-1){
+    string a="";a+=s[i];a+=s[i+1];
+    ap[a]++;
+  }int maxi=0;
+  string apar = "";
+  for(const auto &p : ap){
+    if(p.se>maxi){
+        maxi=p.se;
+        apar=p.fi;
+    }
+  }d(apar);
+}
